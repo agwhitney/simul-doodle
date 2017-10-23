a7a37df31cd45bf53d50f631dd6ab62359ad803c
ag.whitney2@gmail.com <git_Pas5>
2017-10-23 00:46:31 -0400

    Unit moves and is a pygame.sprite object! Needs to be cleaned up in main.py (ie sent to) GameManager and movement needs to be constrained to grid. newUnit.py will replace Unit.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from config import *
 from Managers import GameManager
 from Managers.event_handlers import handle_keys
 from Map.game_map import GameMap
-from Units.Unit import Unit
+from Units.newUnit import Unit
 
 
 if __name__ == '__main__':
@@ -19,34 +19,36 @@ if __name__ == '__main__':
 	ticks = 0
 
 	# MAP AND UNIT INITIALIZE
-	unit = Unit(0, 0, radius=int(TILE_WIDTH / 3))		# Temporaryyyyy
+	unit = Unit()
 	game_map = GameMap(MAP_WIDTH, MAP_HEIGHT, TILE_WIDTH, TILE_HEIGHT)
+
+	units = pygame.sprite.Group()
+	units.add(unit)
 
 	# while ticks < 100 and not done:
 	while not done:
 		for event in pygame.event.get():
+			print(event)
 			quit = handle_keys(event).get('quit')
 			resize = handle_keys(event).get('resize')
-			mmb_down = handle_keys(event).get('mmb_down')
-			mmb_drag = handle_keys(event).get('mmb_drag')
+			lmb_down = handle_keys(event).get('lmb_down')
 
 			if quit:
 				done = True
 			if resize:
 				screen = pygame.display.set_mode((event.w, event.h), RESIZABLE)
-			if mmb_down:
+			if lmb_down:
 				# These can be used elsewhere as long as the button is held
 				x0 = event.pos[0]
 				y0 = event.pos[1]
-				unit.go_to(game_map, x0, y0)
-			if mmb_drag:
-				xf = event.pos[0]
-				yf = event.pos[1]
-				print("A rectangle with sides {} and {}".format(abs(xf - x0), abs(yf - y0)))
+				unit.target_x, unit.target_y = x0, y0
 
+		unit.move()
+		game_map.draw(screen)
+		units.draw(screen)
 		pygame.display.update()  # No args: same as flip
-		ticks += 1
-		if ticks % 10 == 0:
-			GameManager.update(screen, game_map, unit)
-			print(ticks)
+		# ticks += 1
+		# if ticks % 10 == 0:
+		# 	# GameManager.update(screen, game_map, unit)
+		# 	print(ticks)
 		clock.tick(60)  # TODO make this scale w user selected speed somehow
