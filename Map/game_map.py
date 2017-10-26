a15a999edb42c8fdd3ddf64ed4be98b825894cfa
ag.whitney2@gmail.com <git_Pas5>
2017-10-25 23:27:52 -0400

    Unit is moving a little cleaner, with more generality in the Entity class.

--- Map/game_map.py
+++ Map/game_map.py
@@ -3,23 +3,6 @@ import random
 
 
 class GameMap:
-	def random_tile(self, x, y):
-		"""Returns a tile object chosen at random from the tile_list"""
-		tile_number = random.randint(0, len(self.tile_list) - 1)
-		return self.tile_list[tile_number](x * self.tile_width, y * self.tile_height,
-											self.tile_width, self.tile_height)
-
-	def create_new_map(self):
-		"""Returns a new map; an 2d array of tile objects"""
-		new_map = []
-		for x in range(self.width):
-			new_map.append([])
-			row = new_map[x]
-			for y in range(self.height):
-				row.append([])
-				row[y] = self.random_tile(x, y)
-		return new_map
-
 	def __init__(self, width, height, tile_width, tile_height):
 		self.width = width
 		self.height = height
@@ -28,6 +11,24 @@ class GameMap:
 
 		self.tile_list = tile_list()
 		self.map = self.create_new_map()
+
+	def random_tile(self, x, y):
+		"""Returns a tile object chosen at random from the tile_list"""
+		tile_number = random.randint(0, len(self.tile_list) - 1)
+		return self.tile_list[tile_number](x * self.tile_width, y * self.tile_height, self.tile_width, self.tile_height)
+
+	def create_new_map(self):
+		"""Returns a new map; a 2d list of tile objects"""
+		new_map = []
+
+		for x in range(self.width):
+			new_map.append([])
+			row = new_map[x]
+			for y in range(self.height):
+				row.append([])
+				row[y] = self.random_tile(x, y)
+
+		return new_map
 
 	def draw(self, screen):
 		screen.fill((0, 0, 0))
